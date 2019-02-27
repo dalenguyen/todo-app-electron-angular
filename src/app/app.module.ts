fee97b10b7b6d466f1d0061eb0f3c683f7d41b16
Dale Nguyen <dnguyen@ci.com>
2019-02-27 10:57:15 -0500

    Added todo list logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import '../polyfills';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
@@ -33,6 +40,12 @@ export function HttpLoaderFactory(http: HttpClient) {
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    DragDropModule,
     HttpClientModule,
     AppRoutingModule,
     TranslateModule.forRoot({
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
+
+import { MatListOption } from '@angular/material/list';
+
+export interface Todo {
+  title: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent {
+  value = '';
 
-  ngOnInit() {
+  public todo: Todo[] = [
+    { title: 'Watch Bohemian Rhapsody', date: new Date().toString() },
+    { title: 'Read Medium Article', date: new Date().toString()},
+    { title: 'Swimming', date: new Date().toString() }
+  ];
+
+  public done: Todo[] = [
+    { title: 'Write a blog', date: new Date().toString() },
+    { title: 'Study Angular', date: new Date().toString() },
+    { title: 'Check e-mail', date: new Date().toString() },
+    { title: 'Walk dog', date: new Date().toString() }
+  ];
+
+  arr = [];
+
+  onSubmit() {
+    this.todo.push({ title: this.value, date: new Date().toString() });
+    this.value = '';
   }
 
+  clearInput(event: MouseEvent) {
+    if ((<HTMLElement>event.target).nodeName === 'MAT-ICON') {
+      this.value = '';
+    }
+  }
+
+  onAreaListControlChanged(index: number) {
+    setTimeout(() => {
+      const task = this.todo.splice(index, 1);
+      this.done.unshift(task[0]);
+    }, 1000);
+  }
+
+  drop(event: CdkDragDrop<string[]>, type: string) {
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(event.previousContainer.data, event.container.data,
+        event.previousIndex, event.currentIndex);
+    } else {
+      if (type === 'todo') {
+        moveItemInArray(this.todo, event.previousIndex, event.currentIndex);
+      } else {
+        moveItemInArray(this.done, event.previousIndex, event.currentIndex);
+      }
+    }
+  }
 }
