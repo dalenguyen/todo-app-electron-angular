581ac62b3fc1ac196134c774cfa3a449eefd3f3e
Dale Nguyen <dnguyen@ci.com>
2019-02-28 12:47:56 -0500

    Removed redundant code

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
 import {
   CdkDragDrop,
@@ -7,7 +6,6 @@ import {
   transferArrayItem
 } from '@angular/cdk/drag-drop';
 
-import { MatListOption } from '@angular/material/list';
 import { DatabaseService } from '../../providers/database.service';
 
 export interface Todo {
